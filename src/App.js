7f9ab2667605637a98c69cc0e2315cd52bbcd934
Nicolas Schmidt <nicolasdrivebackup@gmail.com>
2023-04-16 18:01:17 +0200

    Loading spinner added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import './App.css';
+import SearchBar from './components/SearchBar';
+import { Container, Spinner, Row } from 'react-bootstrap';
+import { useState } from 'react';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
   return (
-    <div className="App">
-      Podcasts
+    <div 
+      className="d-flex justify-content-center align-items-center mt-5"
+      style={{ minHeight: '100vh' }}
+    >
+      {isLoading ? (
+        <Spinner animation="border" role="status">
+          <span className="visually-hidden">Fetching episodes...</span>
+        </Spinner>
+      ) : (
+        <Container>
+          <SearchBar handleSearch={handleSearch} searchTerm={searchTerm} />
+        </Container>
+
+      )}
+      <SearchBar />
     </div>
   );
 }
