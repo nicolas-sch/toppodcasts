5e0bd76f3bc7e4263ccc2b2c030cd656d98e9315
Nicolas Schmidt <nicolasdrivebackup@gmail.com>
2023-04-16 18:32:25 +0200

    PodcastList component created

---
+++ src/components/PodcastList.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Card, Col } from 'react-bootstrap';
+import { Routes, Route, Link } from 'react-router-dom';
+import EpisodeList from './EpisodeList';
+
+const PodcastList = ({ podcasts }) => {
+  return (
+    <>
+      {podcasts.map((podcast) => (
+        <Col key={podcast.trackId} xs={12} sm={6} md={4} lg={3}>
+          <Card key={podcast.trackId} className="mb-4" data-testid="podcast">
+            <Card.Img variant="top" src={podcast.artworkUrl600} alt={podcast.trackName} />
+            <Card.Body>
+              <Card.Title>{podcast.trackName}</Card.Title>
+              <Card.Text>{podcast.artistName}</Card.Text>
+              <Link to={`podcast/${podcast.trackId}`}>View Episodes</Link>
+              <Routes>
+                <Route
+                  path={`podcast/${podcast.trackId}`}
+                  element={<EpisodeList trackId={podcast.trackId} />}
+                ></Route>
+              </Routes>
+            </Card.Body>
+          </Card>
+        </Col>
+      ))}
+    </>
+  );
+};
+
+export default PodcastList;
